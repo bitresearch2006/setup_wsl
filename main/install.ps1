39673c53f866c7df94cb105719fda159bf92efb2
bitresearch2006 <bitresearch2006@gmail.com>
2026-03-04 07:44:44 +0530

    Update install.ps1 for selected distro

--- main/install.ps1
+++ main/install.ps1
@@ -184,7 +184,7 @@ function Register-WSLAutoStart {
 
     $action = New-ScheduledTaskAction `
         -Execute "cmd.exe" `
-        -Argument '/c start "" "C:\Windows\System32\wsl.exe" -d ubuntu-22.04 --exec sleep 28800'
+        -Argument "/c start "" ""C:\Windows\System32\wsl.exe"" -d $DISTRO --exec sleep 28800"
 
     $trigger = New-ScheduledTaskTrigger -AtLogOn # Changed from AtStartup to AtLogOn
     $trigger.Delay = "PT30S"
